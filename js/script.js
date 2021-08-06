ffeeaa8920252843b5740b77b968f192d54e74d9
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2021-08-06 13:29:41 +0200

    path changes2

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ for (let i = 0; i < footer_links.length; i++) {
 
 
 // loading data from json file via axios package
-const response = await axios("pageCheck/products.json");
+const response = await axios("pageCheck/master/products.json");
 // saving response data into const
 const products = response.data;
 // console.log(products);
